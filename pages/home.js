cee2d6ea48ba120bec966e9fe15aecccaa0e535b
Ratnajit Sarkar <ratnajit.ss@gmail.com>
2022-08-06 02:42:52 +0800

    admin: initial setup testing

---
+++ pages/home.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <Head>
+            <title>SIA AppChallenge</title>
+        </Head>
+        <main>
+            <h1>Welcome Onboard</h1>
+        </main>
+    </>
+  )
+}
+
+export default Home
