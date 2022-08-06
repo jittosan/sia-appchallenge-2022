1e2db831b4ed560844c6a64976e337aac66e738b
Ratnajit Sarkar <ratnajit.ss@gmail.com>
2022-08-06 20:38:10 +0800

    demo on index

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,7 @@ const Home = () => {
             <main className={styles.main}>
                 <h1>WELCOME ONBOARD</h1>
                 <p>Team DJ</p>
-                <br />
-                <Link href={'/demo/redirect'}>Demo (app installed)</Link>
-                <br />
-                <Link href={'/demo'}>Demo (app NOT installed)</Link>
+                <DemoLinksContainer />
                 <div className={styles.brandLogo}>
                     <br />
                 </div>
@@ -26,3 +23,35 @@ const Home = () => {
 }
 
 export default Home
+
+const DemoLinksContainer = () => {
+    return(
+        <div className={styles.demoLinksContainer}>
+        <h1>Protoype Landing Pages</h1>
+        <p>Click on the links below to view our prototype.</p>
+        <br />
+        <p>View our demo video illustrating the key features of our solution.</p>
+        <br />
+        <ul>
+            <Link href={'/demo'}>
+            <li>
+                <h3>New User</h3>
+                <span>Does not have SingaporeAir app on device.</span>
+                <br />
+                <br />
+                <p>Show IFE features accessible to user through app, incentivising them to download and take-up.</p>
+            </li>
+            </Link>
+            <Link href={'/demo/redirect'}>
+            <li>
+                <h3>Existing User</h3>
+                <span>Already has SingaporeAir app on device.</span>
+                <br />
+                <br />
+                <p>Directly bring user to KrisWorld menu, bypassing navigation within app and allowing user direct access to IFE features.</p>
+            </li>
+            </Link>
+        </ul>
+        </div>
+    )
+}
